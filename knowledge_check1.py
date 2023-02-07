72444c5c4dd455e135bfcd0dac539a009fb762a5
Charles McNay <charlesmcnay84@gmail.com>
2023-02-06 20:01:09 -0500

    inital commit

--- knowledge_check1.py
+++ knowledge_check1.py
@@ -1,1 +1,21 @@
 # Knowledge check 1 for code Louisville
+
+print('Hello World!')
+
+# List example
+
+list_test = ["one", 2, "three", 4.65, "five", 6, "seven", 8]
+
+print(list_test[4])
+
+# Dictonary example
+
+about_me = {'age': 38, 'Code Experience': 'Not Enough'}
+
+print(about_me['Code Experience'])
+
+# Tuple example
+
+audio_formats = ('vinyl', 'tape', 'cd', 'mp3')
+
+print(audio_formats[2])
